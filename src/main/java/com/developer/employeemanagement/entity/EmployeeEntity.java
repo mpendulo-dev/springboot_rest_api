b7f183d79b95a65a1a0fa9fb8d1e389d1a9b6311
mpendulo-dev <mpendulo.dev@gmail.com>
2023-05-03 22:07:32 +0200

    Springboot project complete following a YT Tutorial

---
+++ src/main/java/com/developer/employeemanagement/entity/EmployeeEntity.java
@@ -0,0 +1,77 @@
+package com.developer.employeemanagement.entity;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "mt_employee")
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "emp_id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "gener")
+    private String gender;
+
+    @Column(name = "data_of_birth")
+    private Date dateOfBirth;
+
+    @Column(name = "address")
+    private String address;
+
+    public EmployeeEntity() {
+    }
+
+    public EmployeeEntity(long id, String name, String gender, Date dateOfBirth, String address) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
